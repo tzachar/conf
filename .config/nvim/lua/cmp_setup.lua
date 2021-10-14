3dcd73758749428f423843081dd23bbe6e9ac7ba
Nir Tzachar <nir.tzachar@gmail.com>
2021-10-14 08:40:27 +0300

    remove unused code

--- .config/nvim/lua/cmp_setup.lua
+++ .config/nvim/lua/cmp_setup.lua
@@ -52,18 +52,6 @@ local compare_priority = function(entry1, entry2)
 	end
 end
 
-local has_words_before = function()
-	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-		return false
-	end
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
 cmp.setup {
 	snippet = {
 		expand = function(args)
@@ -162,7 +150,6 @@ cmp.setup {
 	preselect = cmp.PreselectMode.None,
 
 	experimental = {
-		custom_menu = true,
 		ghost_text = {
 			hl_group = 'Comment',
 		},
