b8beb7f7b14984ac70c8277bec6376873cad771a
Nir Tzachar <nir.tzachar@gmail.com>
2022-10-18 10:20:41 +0300

    add more sql captures

--- .config/nvim/after/queries/python/injections.scm
+++ .config/nvim/after/queries/python/injections.scm
@@ -1,5 +1,14 @@
 ; extends
+
 (
    (string) @sql
    (#match? @sql "(select|SELECT).*(from|FROM)")
+)
+
+(
+ call
+  function: (attribute attribute: (identifier) @id (#match? @id "execute|read_sql"))
+  arguments: (argument_list
+     (string) @sql
+  )
 )
