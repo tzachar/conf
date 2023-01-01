24e5d7c0128a19f8c0ad5732c76c60534a0398e0
Nir Tzachar <nir.tzachar@gmail.com>
2023-01-01 12:47:10 +0200

    ts: node_incremental: change kyebinding to <m-k,j,i>

--- .config/nvim/plugin/ts_conf.lua
+++ .config/nvim/plugin/ts_conf.lua
@@ -24,10 +24,10 @@ require('nvim-treesitter.configs').setup({
   incremental_selection = {
     enable = true,
     keymaps = { -- mappings for incremental selection (visual mappings)
-      init_selection = 'gnn', -- maps in normal mode to init the node/scope selection
-      node_incremental = 'grn', -- increment to the upper named parent
-      scope_incremental = 'grc', -- increment to the upper scope (as defined in locals.scm)
-      node_decremental = 'grm', -- decrement to the previous node
+      -- init_selection = 'gnn', -- maps in normal mode to init the node/scope selection
+      node_incremental = '<M-k>', -- increment to the upper named parent
+      scope_incremental = '<M-i>', -- increment to the upper scope (as defined in locals.scm)
+      node_decremental = '<M-j>', -- decrement to the previous node
     },
   },
   refactor = {
@@ -271,3 +271,13 @@ vim.api.nvim_create_autocmd('FileType', {
     EnsureTSParserInstalled()
   end
 })
+
+
+-- incremental selection, skip the init_selection function...
+local inc_select = require('nvim-treesitter.incremental_selection')
+vim.keymap.set(
+  'n',
+  '<M-k>',
+  inc_select.init_selection,
+  {silent = true, noremap = true, desc = 'Node incremental selection'}
+)
