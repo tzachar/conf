3cfe029167cff8163cff67133bafc7a6283f1b79
Nir Tzachar <nir.tzachar@gmail.com>
2023-03-05 08:51:21 +0200

    remove plugin

--- .config/nvim/lua/lazy_plugins/misc.lua
+++ .config/nvim/lua/lazy_plugins/misc.lua
@@ -43,24 +43,6 @@ return {
     end,
   },
   {
-    'RRethy/nvim-treesitter-textsubjects',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        textsubjects = {
-          enable = true,
-          prev_selection = ',', -- (Optional) keymap to select the previous selection
-          keymaps = {
-            ['.'] = 'textsubjects-smart',
-            [';'] = 'textsubjects-container-outer',
-            ['i;'] = 'textsubjects-container-inner',
-          },
-        },
-      })
-    end,
-  },
-
-  {
     'RRethy/nvim-treesitter-endwise',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = function()
