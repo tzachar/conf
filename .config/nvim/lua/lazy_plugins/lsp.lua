db82bdd4cd781ef9ca1a199c710a7bbc37b380fd
Nir Tzachar <nir.tzachar@gmail.com>
2023-05-23 17:37:37 +0300

    mason: remind to run pylspinstall after mason update

--- .config/nvim/lua/lazy_plugins/lsp.lua
+++ .config/nvim/lua/lazy_plugins/lsp.lua
@@ -1,9 +1,23 @@
+local function mason_post_install(pkg, _)
+  if pkg.name ~= 'python-lsp-server' then
+    return
+  end
+
+  vim.notify('Restart and run :PylspInstall python-lsp-ruff', vim.log.levels.WARN)
+end
+
 return {
   { 'neovim/nvim-lspconfig' },
   -- update language servers
   {
     'williamboman/mason.nvim',
     dependencies = 'neovim/nvim-lspconfig',
+    build = ":MasonUpdate",
+    cmd = { "Mason", "MasonInstall", "MasonUninstall" },
+    config = function()
+      require("mason").setup()
+      require("mason-registry"):on("package:install:success", vim.schedule_wrap(mason_post_install))
+    end
   },
   {
     'williamboman/mason-lspconfig.nvim',
