bac64e75edc05f974fad54440a484714df51a72e
Nir Tzachar <nir.tzachar@gmail.com>
2024-02-19 11:05:54 +0200

    update injections

--- .config/nvim/after/queries/python/injections.scm
+++ .config/nvim/after/queries/python/injections.scm
@@ -1,14 +1,11 @@
 ; extends
 
-(
-   (string) @sql
-   (#match? @sql "((select|SELECT).*(from|FROM))|((insert|INSERT).*(into|INTO))|((create|CREATE).*(table|TABLE))")
-)
+(string
+  (string_content) @injection.content
+    (#match? @injection.content "((select|SELECT).*(from|FROM))|((insert|INSERT).*(into|INTO))|((create|CREATE).*(table|TABLE))")
+    (#set! injection.language "sql"))
 
-(
- call
+(call
   function: (attribute attribute: (identifier) @id (#match? @id "execute|read_sql"))
   arguments: (argument_list
-     (string (string_content) @sql)
-  )
-)
+    (string (string_content) @injection.content (#set! injection.language "sql"))))
