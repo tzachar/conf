a43715853ce9772d6a7c4e13b0611a50434e2c60
Nir Tzachar <nir.tzachar@gmail.com>
2025-02-18 16:32:40 +0200

    minor

--- .config/nvim/lua/lazy_plugins/snacks.lua
+++ .config/nvim/lua/lazy_plugins/snacks.lua
@@ -7,109 +7,36 @@ return {
       bigfile = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
-      picker = { enabled = true },
-    },
-    lazy = true,
-    keys = {
-      -- telescope
-      {
-        '<leader>b',
-        function()
-          Snacks.picker.buffers({
+      picker = {
+        enabled = true,
+        sources = {
+          buffers = {
             sort_lastused = false,
             reverse = true,
-            -- list = { keys = { ["dd"] = "bufdelete" } },
-          })
-          vim.fn.feedkeys('jj')
-        end,
-        desc = 'Buffers',
+            layout = { preset = "telescope" },
+          },
+        },
       },
-      {
-        '<leader>pp',
-        function()
-          Snacks.picker.smart()
-        end,
-        desc = 'Smart Find Files',
-      },
-      {
-        '<leader>/',
-        function()
-          Snacks.picker.grep()
-        end,
-        desc = 'Grep',
-      },
-      {
-        '<leader>:',
-        function()
-          Snacks.picker.command_history()
-        end,
-        desc = 'Command History',
-      },
-      {
-        '<leader>n',
-        function()
-          Snacks.picker.notifications()
-        end,
-        desc = 'Notification History',
-      },
-      {
-        '<leader>e',
-        function()
-          Snacks.explorer()
-        end,
-        desc = 'File Explorer',
-      },
-      -- LSP
-      {
-        'gd',
-        function()
-          Snacks.picker.lsp_definitions()
-        end,
-        desc = 'Goto Definition',
-      },
-      {
-        'gD',
-        function()
-          Snacks.picker.lsp_declarations()
-        end,
-        desc = 'Goto Declaration',
-      },
-      {
-        'gr',
-        function()
-          Snacks.picker.lsp_references()
-        end,
-        nowait = true,
-        desc = 'References',
-      },
-      {
-        'gI',
-        function()
-          Snacks.picker.lsp_implementations()
-        end,
-        desc = 'Goto Implementation',
-      },
-      {
-        '<leader>gd',
-        function()
-          Snacks.picker.lsp_type_definitions()
-        end,
-        desc = 'Goto T[y]pe Definition',
-      },
-      {
-        '<leader>ss',
-        function()
-          Snacks.picker.lsp_symbols()
-        end,
-        desc = 'LSP Symbols',
-      },
-      {
-        '<leader>sS',
-        function()
-          Snacks.picker.lsp_workspace_symbols()
-        end,
-        desc = 'LSP Workspace Symbols',
-      },
+    },
+    keys = {
+      -- telescope
+      { '<leader>b', function()
+        Snacks.picker.buffers()
+        vim.fn.feedkeys("jj")
+      end, desc = "Buffers" },
+      { '<leader>pp', function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+       -- LSP
+       { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+       { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+       { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+       { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+       { "<leader>gd", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+       -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+       -- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
     },
   },
 }
